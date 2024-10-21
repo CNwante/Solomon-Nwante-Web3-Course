b574b644277917c324a358781fe553c95d439da1
Solomon Nwante <myphilothea@gmail.com>
2024-10-21 17:10:09 +0100

    Refactor and add tests

--- module-1/functions/recursion.js
+++ module-1/functions/recursion.js
@@ -1,14 +1,15 @@
 // Recursion Exercise
 
 function isEven(num) {
-  if (num < 0) {
-    return "Number must be a positive whole number.";
-  }
-  if (num === 0) {
-    return true;
-  }
-  if (num === 1) {
-    return false;
-  }
+  if (num < 0) return "Number must be a positive whole number.";
+  if (num === 0) return true;
+  if (num === 1) return false;
   return isEven(num - 2);
 }
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75.5));
+// → false
+console.log(isEven(-1));
+// → Number must be a positive whole number.
