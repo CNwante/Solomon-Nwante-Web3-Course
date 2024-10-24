3510597b5e57e4ed0c9ae3f2f9db78fec5dae11b
Solomon Nwante <myphilothea@gmail.com>
2024-10-24 10:32:03 +0100

    Implement custom loop

---
+++ module-2/your-own-loop.js
@@ -0,0 +1,15 @@
+// Your Own Loop Implementation
+
+function loop(value, test, update, body) {
+  let currentValue = value;
+  while (test(currentValue)) {
+    body(currentValue);
+    currentValue = update(currentValue);
+  }
+  return currentValue;
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
