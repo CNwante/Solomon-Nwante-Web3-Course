3a6b25a40b59373a5daad443b11a1dd676e0337b
Solomon Nwante <myphilothea@gmail.com>
2024-11-08 15:50:06 +0100

    Detect the metamask provider

---
+++ module-4/simple-dapp/src/detect.js
@@ -0,0 +1,20 @@
+import detectEthereumProvider from "@metamask/detect-provider";
+
+async function setup() {
+  const provider = await detectEthereumProvider();
+
+  if (provider && provider === window.ethereumm) {
+    console.log("MetaMask is available!");
+    startApp(provider);
+  } else {
+    console.log("Please install MetaMask!");
+  }
+}
+
+function startApp(provider) {
+  if (provider !== window.ethereum) {
+    console.error("Do you hav multiple wallets installed?");
+  }
+}
+
+window.addEventListener("load", setup);
